3c32338ce6842fc04839fd2174fd4c1468892f82
Erik Tejeda <etrq@protonmail.com>
2021-10-29 15:33:00 -0500

    commit 2

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=11:antialias=false:autohint=false";
 static int borderpx = 2;
 
 /*
